445ebc26e876426a9d8b4f440ab22b951fb202aa
Luca Santarella <luca.santarella@gmail.com>
2021-04-27 14:24:14 -0400

    Cleanup

--- main.py
+++ main.py
@@ -26,32 +26,33 @@ def get_dashboard_url(account_id, dashboard_id, quicksight_user_arn, quicksight_
     :param undo_redo_disabled:
     :return:
     """
-    try:
-        return qs.get_dashboard_embed_url(
-            AwsAccountId=account_id,
-            DashboardId=dashboard_id,
-            IdentityType='QUICKSIGHT',
-            Namespace=quicksight_namespace,
-            SessionLifetimeInMinutes=600,
-            UndoRedoDisabled=undo_redo_disabled,
-            ResetDisabled=reset_disabled,
-            UserArn=quicksight_user_arn
-        )
-    except ClientError as e:
-        return "Error generating embeddedURL: " + str(e)
+    return qs.get_dashboard_embed_url(
+        AwsAccountId=account_id,
+        DashboardId=dashboard_id,
+        IdentityType='QUICKSIGHT',
+        Namespace=quicksight_namespace,
+        SessionLifetimeInMinutes=600,
+        UndoRedoDisabled=undo_redo_disabled,
+        ResetDisabled=reset_disabled,
+        UserArn=quicksight_user_arn
+    )
 
 
 @app.route('/embed/<string:dashboard_id>')
 def get(dashboard_id):
-    embed_data = get_dashboard_url(
-        os.getenv('AWS_ACCOUNT_ID'),
-        dashboard_id,
-        os.getenv('AWS_QUICKSIGHT_USER_ARN'),
-        os.getenv('AWS_QUICKSIGHT_NAMESPACE'),
-        False,
-        False
-    )
-    return render_template('embed.html', embed_url=embed_data['EmbedUrl'])
+    try:
+        embed_data = get_dashboard_url(
+            os.getenv('AWS_ACCOUNT_ID'),
+            dashboard_id,
+            os.getenv('AWS_QUICKSIGHT_USER_ARN'),
+            os.getenv('AWS_QUICKSIGHT_NAMESPACE'),
+            False,
+            False
+        )
+        return render_template('embed.html', embed_url=embed_data['EmbedUrl'])
+    except ClientError as e:
+        print(e)
+
 
 if __name__ == "__main__":
     app.run(port=int(os.getenv('HTTP_PORT', 5050)))
